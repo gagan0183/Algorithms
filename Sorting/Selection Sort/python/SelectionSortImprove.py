27eb7554946983b951ad2d59bc37ee7cf40a17c7
Gagandeep singh <g.deepsingh1@gmail.com>
2018-10-19 02:18:02 +0530

    Delete SelectionSortImprove.py

--- Sorting/Selection Sort/python/SelectionSortImprove.py
+++
@@ -1,7 +0,0 @@
-def SelectionSort(l):
-  for start in range(len(l)):
-    minpos = start
-    for i in range(start, len(l)):
-      if l[i] < l[minpos]:
-        minpos = l
-    (l[start],l[minpos])=(l[minpos],l[start])
